53ea219c90ba2e6847383cca71393ecbebca1f78
Fernanda <fernanda_cader@outlook.com>
2022-10-28 23:44:45 -0300

    Acerca de

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,4 +1,13 @@
 export const environment = {
+  firebase: {
+    projectId: 'frontendpruebafc',
+    appId: '1:129195645848:web:1eb1bbfc630491c19969ed',
+    storageBucket: 'frontendpruebafc.appspot.com',
+    locationId: 'southamerica-east1',
+    apiKey: 'AIzaSyAbKdTZRzaORVhDvV4bT66vSogbPfSj33k',
+    authDomain: 'frontendpruebafc.firebaseapp.com',
+    messagingSenderId: '129195645848',
+  },
   production: true,
   URL: 'https://backendfer.herokuapp.com/'
 };
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -3,6 +3,15 @@
 // The list of file replacements can be found in `angular.json`.
 
 export const environment = {
+  firebase: {
+    projectId: 'frontendpruebafc',
+    appId: '1:129195645848:web:1eb1bbfc630491c19969ed',
+    storageBucket: 'frontendpruebafc.appspot.com',
+    locationId: 'southamerica-east1',
+    apiKey: 'AIzaSyAbKdTZRzaORVhDvV4bT66vSogbPfSj33k',
+    authDomain: 'frontendpruebafc.firebaseapp.com',
+    messagingSenderId: '129195645848',
+  },
   production: false,
   URL: 'http://localhost:8080/',
   
